428c1d911dd58d642baf43f2bbad123932ef3015
David Francoeur <dfrancoeur04@gmail.com>
2023-12-19 09:23:52 -0500

    compiles on scala3

--- smithy-playground.scala
+++ smithy-playground.scala
@@ -1,39 +1,23 @@
-//> using scala "2.13.10"
-//> using lib "software.amazon.smithy:smithy-model:1.27.0"
+//> using scala "3.3.1"
+//> using lib "software.amazon.smithy:smithy-model:1.42.0"
 
 import software.amazon.smithy.model.Model
 import scala.jdk.CollectionConverters._
 import software.amazon.smithy.model.shapes.ShapeId
 import software.amazon.smithy.model.traits.DefaultTrait
+import software.amazon.smithy.model.shapes.Shape
+import software.amazon.smithy.model.shapes.StringShape
+import software.amazon.smithy.model.shapes.IntegerShape
+import software.amazon.smithy.model.shapes.AbstractShapeBuilder
+import software.amazon.smithy.model.shapes.IntegerShape.Builder
 
 object Main extends App {
-  val model = Model
-    .assembler()
-    .addUnparsedModel(
-      "defaults.smithy",
-      s"""|namespace atest
-          |
-          |structure DirectInt {
-          |  member: Integer
-          |}
-          |
-          |@range(min: 1)
-          |integer MyInt
-          |structure IndirectInt {
-          |  member: MyInt
-          |}
-          |""".stripMargin
-    )
-    .assemble()
-    .unwrap()
-  println(
-    model.expectShape(ShapeId.from("atest#DirectInt$member")).getAllTraits()
-  ) // {}
-  val indirect = model.expectShape(ShapeId.from("atest#IndirectInt$member"))
-  println(
-    indirect.getAllTraits()
-  ) // {smithy.api#default=Trait `smithy.api#default`, defined at defaults.smithy [10, 3]}
-  println(
-    indirect.getTrait(classOf[DefaultTrait]).get().toNode()
-  ) // 0
+  def test(id: String): Shape = {
+    val builder: AbstractShapeBuilder[_, _] = id match {
+      case "test"  => StringShape.builder()
+      case "other" => IntegerShape.builder().addMixin(null)
+      case _       => IntegerShape.builder()
+    }
+    builder.id(id).build()
+  }
 }
