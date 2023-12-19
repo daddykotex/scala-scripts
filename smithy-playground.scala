66356a202fc2719a0cade8986a087f5b59b0ea57
David Francoeur <dfrancoeur04@gmail.com>
2023-12-19 13:36:29 -0500

    repro

--- smithy-playground.scala
+++ smithy-playground.scala
@@ -2,14 +2,10 @@
 //> using lib "software.amazon.smithy:smithy-model:1.42.0"
 
 import software.amazon.smithy.model.Model
-import scala.jdk.CollectionConverters._
-import software.amazon.smithy.model.shapes.ShapeId
-import software.amazon.smithy.model.traits.DefaultTrait
+import software.amazon.smithy.model.shapes.AbstractShapeBuilder
+import software.amazon.smithy.model.shapes.IntegerShape
 import software.amazon.smithy.model.shapes.Shape
 import software.amazon.smithy.model.shapes.StringShape
-import software.amazon.smithy.model.shapes.IntegerShape
-import software.amazon.smithy.model.shapes.AbstractShapeBuilder
-import software.amazon.smithy.model.shapes.IntegerShape.Builder
 
 object Main extends App {
   def test(id: String): Shape = {
@@ -18,6 +14,11 @@ object Main extends App {
       case "other" => IntegerShape.builder().addMixin(null)
       case _       => IntegerShape.builder()
     }
+    // the following works
     builder.id(id).build()
+
+    // the following two do not work
+    // builder.id(id)
+    // builder.build()
   }
 }
